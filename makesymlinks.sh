d7f9ed4b4c0b1ca8fd8ec00be022358b81381744
Helge Liebert <helge.liebert@googlemail.com>
2016-09-19 12:57:07 +0200

    dotfiles-old folder now hidden

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,7 +7,7 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles-old             # old dotfiles backup directory
+olddir=~/.dotfiles-old             # old dotfiles backup directory
 #files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 files="bashrc vimrc zshrc spacemacs xprofile rundo.sh Rprofile vimperatorrc"    # list of files/folders to symlink in homedir
 
@@ -26,7 +26,7 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles-old/
+    mv ~/.$file ~/.dotfiles-old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
