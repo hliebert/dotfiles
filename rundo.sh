0b3bf9ed0aa850b971c1420f349f665de82d485a
Helge Liebert <helge.liebert@googlemail.com>
2017-08-29 10:23:22 +0200

    Cleaned changes.

--- rundo.sh
+++ rundo.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# script needs wmctrl, xsel and xdotool 
+# script needs xsel and xdotool 
 # to get them run
 # in debian/ubuntu linux
-# sudo apt-get install wmctrl xsel xdotool
+# sudo apt-get install xsel xdotool
 # in arch
-# sudo pacman -S wmctrl xsel xdotool
-# the default delays should work on most systems 
+# sudo pacman -S xsel xdotool
 
 # save clipboard contents
 clip=$(xsel -b)
@@ -14,39 +13,26 @@ clip=$(xsel -b)
 echo 'do ' '"'$1'"' | xsel -b
 
 # get current window id or name
-#winid=$(xdotool getactivewindow getwindowname)
 winid=$(xdotool getactivewindow)
 # get last active stata window id 
 stataid=$(xdotool search --name "Stata" | tail -1)
 
 
 # check for stata window, if found activate else execute
-#if [ "$(pgrep stata)" ] 
+# switch to command window, ctrl-1 in Stata v11+ (ctrl-4 in stata 10) 
+# and select any existing text via ctrl-a, then paste over it and execute
 if [ "$stataid" ] 
 then
-    #wmctrl -a 'Stata/MP 13.1'
     xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
-    #xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' type 'do ' '"'$1'"'
-    #xdotool key 'Return'
 else
     xstata-mp &
     stataid=$(xdotool search --sync --name "Stata" | tail -1)
-    #sleep .2
     xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
-    #xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' type 'do ' '"'$1'"'
-    #xdotool key 'Return'
 fi
-
-# switch to command window, ctrl-1 in Stata v11+ (ctrl-4 in stata 10) 
-# and select any existing text via ctrl-a, then paste over it and execute
-#xdotool key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
-
 
 # restore clipboard
 echo $clip | xsel -b
 
 # go back to editor window
-#sleep .3
-#wmctrl -a $winid 
 xdotool windowactivate --sync $winid
 
