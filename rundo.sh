688edabf9af578dc0ea88a1a6935f99c8e98ed16
Helge Liebert <helge.liebert@gmail.com>
2018-08-29 18:47:03 +0200

    xdotool does not work properly in wayland anymore

--- rundo.sh
+++ rundo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# script needs xsel and xdotool 
+# script needs xsel and xdotool
 # to get them run
 # in debian/ubuntu linux
 # sudo apt-get install xsel xdotool
@@ -13,26 +13,45 @@ clip=$(xsel -b)
 echo 'do ' '"'$1'"' | xsel -b
 
 # get current window id or name
+# winid=$(xdotool getactivewindow getwindowname)
 winid=$(xdotool getactivewindow)
-# get last active stata window id 
+# get last active stata window id
 stataid=$(xdotool search --name "Stata/MP" | tail -1)
 
 
 # check for stata window, if found activate else execute
-# switch to command window, ctrl-1 in Stata v11+ (ctrl-4 in stata 10) 
+# switch to command window, ctrl-1 in Stata v11+ (ctrl-4 in stata 10)
 # and select any existing text via ctrl-a, then paste over it and execute
-if [ "$stataid" ] 
+if [ "$stataid" ]
 then
-    xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
+    # xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
+    wmctrl -a 'Stata/MP'
+    # sleep 0.1
+    xte 'keydown Control_L' 'key 1' 'key A' 'usleep 100' \
+        'key V' 'keyup Control_L'
+    sleep 0.1
+    xte 'key Return'
 else
+    # xstata-mp &
+    # stataid=$(xdotool search --sync --name "Stata" | tail -1)
+    # xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
     xstata-mp &
-    stataid=$(xdotool search --sync --name "Stata" | tail -1)
-    xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
+    sleep 0.2
+    wmctrl -a 'Stata/MP'
+    # sleep 0.1
+    xte 'keydown Control_L' 'key 1' 'key A' 'usleep 100' \
+        'key V' 'keyup Control_L'
+    sleep 0.1
+    xte 'key Return'
 fi
+
+
+
 
 # restore clipboard
 echo $clip | xsel -b
 
 # go back to editor window
-xdotool windowactivate --sync $winid
-
+# xdotool windowactivate --sync $winid
+# wmctrl -a $winid
+wmctrl -ia $winid
