b1579d71359648e2d27e8943c53b9bff96cefcc9
Helge Liebert <helge.liebert@gmail.com>
2018-08-29 19:09:44 +0200

    Currently not using those.

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/dotfiles/doom.d/packages.el
 
-(package! org-projectile)
-(package! hledger-mode)
+;; (package! org-projectile)
+;; (package! hledger-mode)
 (package! academic-phrases)
 (package! ado-mode :recipe (:fetcher github :repo "louabill/ado-mode"
                                      :files ("lisp/*.el")))
