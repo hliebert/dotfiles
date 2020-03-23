c9f323d7b39152c5284278285d228c61c8e6fa65
Helge Liebert <helge.liebert@gmail.com>
2020-03-23 09:33:56 +0100

    Remove packages.

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -1,35 +1,14 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/dotfiles/doom.d/packages.el
 
-;; (package! org-projectile)
-;; (package! hledger-mode)
-;; (package! exec-path-from-shell)
-(package! academic-phrases)
-(package! yasnippet-snippets)
-(package! transpose-frame)
-(package! banner-comment)
-(package! org-projectile)
 (package! rebecca-theme)
+(package! academic-phrases)
+(package! banner-comment)
 (package! dired-hacks)
-(package! poly-R)
-;; (package! rebox2)
-;; (package! ranger)
+(package! yasnippet-snippets)
 (package! eterm-256color)
+;; (package! transpose-frame)
+;; (package! org-projectile)
 
-(package! ado-mode :recipe (:host github :repo "louabill/ado-mode"
-                                     :files ("lisp/*.el")))
-
-(package! emacs-snippets
-  :recipe (:host github
-           :repo "hlissner/emacs-snippets"
-           :files ("*")))
-
-;; (package! yasnippet-snippets
-;;   :recipe (:host github
-;;            :repo "AndreaCrotti/yasnippet-snippets"
-;;            :files ("*")))
-
-(package! rebox2
-  :recipe (:host github
-           :repo "lewang/rebox2"
-           :files ("rebox2.el")))
+(package! ado-mode
+  :recipe (:host github :repo "louabill/ado-mode"))
