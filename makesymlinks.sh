19305899b43ec4e0e9731cc671832573dcf48068
Helge Liebert <helge.liebert@gmail.com>
2020-03-24 11:41:46 +0100

    Simplified, removed broken zsh installation part.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Notes
-# for doom, add (doom! :config private) to ~/.emacs.d/init.el
+# =============================== Setup dotfiles ===============================
 
-########## Variables
+# This script creates symlinks from the home directory to any desired dotfiles
+# in ~/dotfiles
 
-dir=${HOME}/dotfiles                    # dotfiles directory
-olddir=${HOME}/.dotfiles-old            # old dotfiles backup directory
-files="bashrc bash_profile vimrc zshrc zshenv doom.d spacemacs xprofile rundo.sh Rprofile latexmkrc editorconfig"    # list of files/folders to symlink in homedir
-
-##########
+# dotfiles directory
+dir=${HOME}/dotfiles
+# old dotfiles backup directory
+olddir=${HOME}/.dotfiles-old
+# list of files/folders to symlink in homedir
+files="bashrc bash_profile vimrc zshrc zshenv doom.d spacemacs xprofile rundo.sh Rprofile latexmkrc editorconfig"
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 echo "done"
 
-# create backupdir for vim 
+# create backupdir for vim
 echo -n "Creating Vim backupdir/undodir in ~ ..."
 mkdir -p ${HOME}/.backup-vim
 echo "done"
@@ -30,47 +27,12 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory, then create
+# symlinks from the homedir to any files in the ~/dotfiles directory specified
+# in $files
 for file in $files ; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ${HOME}/.$file ${HOME}/.dotfiles-old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ${HOME}/.$file
 done
-
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # install antigen
-    curl -L git.io/antigen > antigen.zsh
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/arch-release ]]; then
-            sudo pacman -S zsh
-            install_zsh
-        fi
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
-        fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
-        fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
-}
-
-install_zsh
-
