4e10a4c82b2015c44bd860bb918c060abde89cd3
Helge Liebert <helge.liebert@gmail.com>
2020-04-30 19:09:14 +0200

    Updates, ditch workspaces.

--- doom.d/init.el
+++ doom.d/init.el
@@ -43,14 +43,14 @@
        ;;neotree             ; a project drawer, like NERDTree for vim
        ophints               ; highlight the region an operation acts on
        (popup +defaults)     ; tame sudden yet inevitable temporary windows
-       ;;pretty-code         ; replace bits of code with pretty symbols
+       ;;pretty-code         ; ligatures or substitute text with pretty symbols
        ;;tabs                ; an tab bar for Emacs
        treemacs              ; a project drawer, like neotree but cooler
        ;;unicode             ; extended unicode support for various languages
        vc-gutter             ; vcs diff in the fringe
        vi-tilde-fringe       ; fringe tildes to mark beyond EOB
        window-select         ; visually switch windows
-       workspaces            ; tab emulation, persistence & separate workspaces
+       ;;workspaces          ; tab emulation, persistence & separate workspaces
        ;;zen                 ; distraction-free coding or writing
 
        :editor
@@ -73,13 +73,14 @@
         +icons)              ; colorful icons for dired-mode
        electric              ; smarter, keyword-based electric-indent
        (ibuffer +icons)      ; interactive buffer management
+       (undo  +tree)         ; persistent, smarter undo for your inevitable mistakes
        vc                    ; version-control and Emacs, sitting in a tree
 
        :term
-       eshell                ; a consistent, cross-platform shell (WIP)
-       ;; shell              ; a terminal REPL for Emacs
-       term                  ; terminals in Emacs
-       ;; vterm              ; another terminals in Emacs
+       eshell                ; the elisp shell that works everywhere
+       ;;shell               ; simple shell REPL for Emacs
+       term                  ; basic terminal emulator for Emacs
+       ;;vterm               ; the best terminal emulation in Emacs
 
        :checkers
        (syntax               ; tasing you for every semicolon you forget
@@ -100,7 +101,7 @@
         +dictionary
         +offline
         +docsets)           
-       lsp
+       (lsp +peek)
        ;;macos               ; MacOS-specific commands
        magit                 ; a git porcelain for Emacs
        ;;make                ; run make tasks from Emacs
@@ -122,6 +123,7 @@
        ;;crystal             ; ruby at the speed of c
        ;;csharp              ; unity, .NET, and mono shenanigans
        data                  ; config/data formats
+       ;;(dart +flutter)     ; paint ui and not much else
        ;;elixir              ; erlang done right
        ;;elm                 ; care for a cup of TEA?
        emacs-lisp            ; drown in parentheses
@@ -140,6 +142,7 @@
        ;;kotlin              ; a better, slicker Java(Script)
        (latex                ; writing papers in Emacs has never been so fun
         +latexmk
+        +lsp
         +cdlatex)
        ;;lean
        ;;factor
@@ -165,11 +168,12 @@
        ;;racket              ; a DSL for DSLs
        ;;rest                ; Emacs as a REST client
        ;;rst                 ; ReST in peace
-       ;;ruby                ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;(ruby +rails)       ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust                ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala               ; java, but good
        ;;scheme              ; a fully conniving family of lisps
        sh                    ; she sells {ba,z,fi}sh shells on the C xor
+       ;;sml
        ;;solidity            ; do you need a blockchain? No.
        ;;swift               ; who asked for emoji variables?
        ;;terra               ; Earth and Moon in alignment for performance.
@@ -178,7 +182,7 @@
         +html)               ;
 
        :email
-       ;;(mu4e +gmail)
+       (mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
@@ -190,6 +194,6 @@
 
        :config
        ;;literate
-       (default +bindings +smartparens))
+       (default +bindings))
 
 (setq evil-respect-visual-line-mode t)
