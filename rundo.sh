303b72973da6a6553aa05b8d0cba797a3f8b236b
Helge Liebert <helge.liebert@gmail.com>
2020-11-03 15:56:17 +0100

    Simiplified and updated this again.

--- rundo.sh
+++ rundo.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# script needs xsel and xdotool
-# to get them run
-# in debian/ubuntu linux
+# debian/ubuntu linux
 # sudo apt-get install xsel xdotool xautomation wmctrl
-# in arch
-# sudo pacman -S xsel xdotool
 
 # save clipboard contents
 clip=$(xsel -b)
@@ -16,46 +12,23 @@ echo 'do ' '"'$1'"' | xsel -b
 # winid=$(xdotool getactivewindow getwindowname)
 winid=$(xdotool getactivewindow)
 # get last active stata window id
-# stataid=$(xdotool search --name "Stata/MP" | tail -1)
-
+stataid=$(xdotool search --name "Stata/" | tail -1)
 
 # check for stata window, if found activate else execute
 # switch to command window, ctrl-1 in Stata v11+ (ctrl-4 in stata 10)
 # and select any existing text via ctrl-a, then paste over it and execute
-# if [ "$stataid" ]
-# then
-    # xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
-    wmctrl -a 'Stata/'
-    # wmctrl -ia $stataid
-    sleep 0.1
-    xte 'keydown Control_L' 'key 1' 'key A' 'usleep 100' \
-        'key V' 'keyup Control_L'
-    sleep 0.1
-    xte 'key Return'
-# else
-    # # xstata-mp &
-    # # stataid=$(xdotool search --sync --name "Stata" | tail -1)
-    # # xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
-    # xstata-mp &
-    # sleep 0.2
-    # wmctrl -a 'Stata/MP'
-    # # wmctrl -ia $stataid
-    # # sleep 0.1
-    # xte 'keydown Control_L' 'key 1' 'key A' 'usleep 100' \
-        # 'key V' 'keyup Control_L'
-    # sleep 0.1
-    # xte 'key Return'
-# fi
-
-
-
+if [ "$stataid" ]
+then
+    xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
+else
+    xstata-se &
+    stataid=$(xdotool search --sync --name "Stata" | tail -1)
+    xdotool windowactivate --sync $stataid key 'Control_L+1' 'Control_L+a' 'Control_L+v' 'Return'
+fi
 
 # restore clipboard
-# echo $clip | xsel -b
+echo -n "$clip" | xsel -b
 
 # go back to editor window
 sleep 0.1
 xdotool windowactivate --sync $winid
-# sleep 0.3
-# wmctrl -a $winid
-# wmctrl -ia $winid
